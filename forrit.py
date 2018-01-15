37a1a9a70732801f64dcf167043ce258468e1d38
ingofreak <33750816+ingofreak@users.noreply.github.com>
2018-01-15 12:23:40 +0000

    Update forrit.py

--- forrit.py
+++ forrit.py
@@ -3,7 +3,17 @@ import os
 
 @route('/')
 def index():
-  return "<a href="www.vefsida-ingo.herokuapp.com/about">Bottlepy</a> virkar, alveg rosa gaman..."
+  return "Bottlepy virkar, alveg rosa gaman...<br><a href="www.vefsida-ingo.herokuapp.com/about">about</a>
+<a href="www.vefsida-ingo.herokuapp.com/pic">pic</a><a href="www.vefsida-ingo.herokuapp.com/bio">bio</a>"
+@route('/about')
+def about():
+  return "þetta er about"
+@route('/pic')
+def pic():
+  return "Þetta er fyrir myndir"
+@route('/bio')
+def bio():
+  return "Þetta er bio"
   
 #run()
 run(host="0.0.0.0", port=os.environ.get('PORT'))
